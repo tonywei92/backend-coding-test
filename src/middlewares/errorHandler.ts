950e27441ac4dc2c65e1a7901564545a586bdaa2
Tony Song <tonywei92@gmail.com>
2020-09-04 05:54:18 +0700

    chore: add winston logger on expressererror handler middleware

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -2,6 +2,7 @@
 import { Request, NextFunction } from 'express';
 import { ResponseObj } from './createResponse';
 import HttpException from '../exceptions/HttpException';
+import logger from '../logger';
 
 const errorHandler = (
   err: HttpException,
@@ -9,6 +10,7 @@ const errorHandler = (
   res: ResponseObj,
   next: NextFunction
 ): void => {
+  logger.error(err);
   res
     .status(err.code || 500)
     .json(res.createResponse(null, 'error', err.message));
